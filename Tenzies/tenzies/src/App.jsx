6b00cd774ba93bfe282def187b04a00ae05d9ae6
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-10-09 16:31:24 -0500

    Update UI and add functionality

--- Tenzies/tenzies/src/App.jsx
+++ Tenzies/tenzies/src/App.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Die from './components/Die.jsx';
 
 const App = () => {
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  const diceElements = dice.map((num) => <Die key={Math.random()} value={num} />);
+
+  const rollDice = () => {
+    setDice(generateAllNewDice())
+  }
+
   return (
     <main>
-      <div className='dice-container'>
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-      </div>
+      <div className="dice-container">{diceElements}</div>
+
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
     </main>
   );
 };
