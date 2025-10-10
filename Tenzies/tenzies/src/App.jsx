37ce813f23bc034c176b932acc1a663d54805526
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-10-10 16:24:03 -0500

    Update and Practice State in Tenzies App

--- Tenzies/tenzies/src/App.jsx
+++ Tenzies/tenzies/src/App.jsx
@@ -9,17 +9,21 @@ const App = () => {
     return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
   }
 
-  const diceElements = dice.map((num) => <Die key={Math.random()} value={num} />);
+  const diceElements = dice.map((num) => (
+    <Die key={Math.random()} value={num} />
+  ));
 
   const rollDice = () => {
-    setDice(generateAllNewDice())
-  }
+    setDice(generateAllNewDice());
+  };
 
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
 
-      <button className='roll-dice' onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 };
