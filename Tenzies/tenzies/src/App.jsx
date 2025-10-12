fe4885b864ef1460650bca49d670655fbf753ce3
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-10-12 14:35:48 -0500

    Change Dice to objects and Hold Dice functionality

--- Tenzies/tenzies/src/App.jsx
+++ Tenzies/tenzies/src/App.jsx
@@ -6,16 +6,33 @@ const App = () => {
   const [dice, setDice] = useState(generateAllNewDice());
 
   function generateAllNewDice() {
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: Math.random(),
+    }));
   }
-
-  const diceElements = dice.map((num) => (
-    <Die key={Math.random()} value={num} />
-  ));
 
   const rollDice = () => {
     setDice(generateAllNewDice());
   };
+
+  const hold = (id) => {
+    setDice((oldDice) => {
+      return oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      });
+    });
+  };
+
+  const diceElements = dice.map((dieObj) => (
+    <Die
+      key={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={() => hold(dieObj.id)}
+    />
+  ));
 
   return (
     <main>
--- Tenzies/tenzies/src/components/Die.jsx
+++ Tenzies/tenzies/src/components/Die.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './Die.css';
 
 const Die = (props) => {
-  return <button>{props.value}</button>;
+  const styles = {
+    backgroundColor: props.isHeld ? '#59e391' : '#FFF',
+  };
+
+  return (
+    <button style={styles} onClick={props.hold}>
+      {props.value}
+    </button>
+  );
 };
 
 export default Die;
