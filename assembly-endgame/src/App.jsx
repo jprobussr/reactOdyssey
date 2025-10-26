ec8305ead880a3084d53547d23494c67bb0b1132
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-10-26 16:13:02 -0500

    Initial Setup for Assembly Endgame Capstone Project

---
+++ assembly-endgame/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <header>
+        <h1>Assembly Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+
+      <section className='game-status'>
+        <h2>You Win!</h2>
+        <p>Well Done! 🥳</p>
+      </section>
+    </main>
+  );
+};
+
+export default App;
