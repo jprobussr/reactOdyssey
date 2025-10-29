31ff3064cef8f5f948db45ffa0dfc85f19daf2a0
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-10-29 16:09:30 -0500

    Update UI for Endgame

--- assembly-endgame/src/App.jsx
+++ assembly-endgame/src/App.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import './App.css';
+import { languages } from './languages.js';
 
-const App = () => {
+const AssemblyEndgame = () => {
+  const langEl = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+
+    return (
+      <span className='chips' style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    )
+  });
+
   return (
     <main>
       <header>
-        <h1>Assembly Endgame</h1>
+        <h1>Assembly: Endgame</h1>
         <p>
           Guess the word within 8 attempts to keep the programming world safe
           from Assembly!
         </p>
       </header>
 
-      <section className='game-status'>
+      <section className="game-status">
         <h2>You Win!</h2>
         <p>Well Done! 🥳</p>
+      </section>
+
+      <section className='language-chips'>
+        {langEl}
       </section>
     </main>
   );
 };
 
-export default App;
+export default AssemblyEndgame;
