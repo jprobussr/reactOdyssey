e3b2f3037a8f396bf4b6b4f9660143071491f273
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-11-20 16:24:34 -0600

    Practice useContext() and writing App, Header, and Button components

--- theme-switcher/src/App.jsx
+++ theme-switcher/src/App.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import Header from './components/Header.jsx';
-import Button from './components/Button.jsx';
-import './App.css';
+import Button from './components/Button.jsx'
+import './App.css'
 
 const ThemeContext = React.createContext();
+
 const App = () => {
   const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-  };
+    setTheme((prev) => prev === 'light' ? 'dark' : 'light');
+  }
+
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <div className={`${theme}-theme`}>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <div className={`container ${theme}-theme`}>
         <Header />
         <Button />
       </div>
@@ -21,4 +23,5 @@ const App = () => {
 };
 
 export default App;
-export { ThemeContext };
+
+export { ThemeContext }
